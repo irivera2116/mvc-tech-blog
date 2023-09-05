3365a445cda1ec7b3f641add6931de6b40f7ac71
irivera2116 <irivera2116@GMAIL.COM>
2023-09-05 15:55:56 -0400

    added code to login/logout

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Login failed.");
+    }
+  }
+};
+
+const loginForm = document.querySelector(".login-form");
+if (loginForm) {
+  loginForm.addEventListener("submit", loginHandler);
+};
